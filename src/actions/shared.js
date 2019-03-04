13cd3622a979c89ac0d0f3777e069b38e8b9709e
Eric Njogu <kunadawa@gmail.com>
2019-03-04 10:14:50 +0300

    feat: create actions and async action handlers
    receive initial data (users, tweets), set active (authed) user

---
+++ src/actions/shared.js
@@ -0,0 +1,19 @@
+import {getInitialData} from '../utils/api'
+import {receiveTweets} from './tweets'
+import {receiveUsers} from './users'
+import {setAuthedUser} from './authUser'
+
+const AUTHED_ID = 'dan_abramov'
+
+export function handleInitialData() {
+    return (dispatch) => {
+        return getInitialData().then(
+            ({users, tweets}) => {
+                dispatch(receiveUsers(users))
+                dispatch(receiveTweets(tweets))
+                dispatch(setAuthedUser(AUTHED_ID))
+            }
+        )
+    }
+
+}
