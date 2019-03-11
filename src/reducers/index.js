8c74c35e0fcfd86bdaf9aa24ea2e227cb2532acd
Eric Njogu <kunadawa@gmail.com>
2019-03-11 11:50:56 +0300

    fix: changing import statements to reflect the default exports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
-import {authedUser} from './autherUser'
-import {tweets} from './tweets'
-import  {users} from './users'
+import authedUser from './authedUser'
+import tweets from './tweets'
+import  users from './users'
 import {combineReducers} from 'redux'
 
 export default combineReducers({authedUser, users, tweets})
