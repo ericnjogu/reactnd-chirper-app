c69158019150e6cfc23e70b226eb3879de2afeed
Eric Njogu <kunadawa@gmail.com>
2019-03-14 10:27:03 +0300

    feat: displaying tweet data in render()

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {formatTweet} from '../utils/helpers'
+import {formatTweet, formatDate} from '../utils/helpers'
+import {TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline} from 'react-icons/ti'
 
 class Tweet extends Component {
+    toParent = (e, id) => {
+        e.preventDefault()
+        // TODO
+    }
+
+    handleLike = (e) => {
+        e.preventDefault()
+        //TODO
+    }
+
     render() {
+        const {name, avatar, timestamp, text, hasLiked, likes, replies, id, parent} = this.props.tweet
         return (
             <div className='tweet'>
-
+                <img src={avatar} alt={`Avatar of ${name}`} className='avatar'/>
+                <div className='tweet-info'>
+                    <div>
+                        <span>{name}</span>
+                        <div>{formatDate(timestamp)}</div>
+                        {parent && (
+                            <button className='replying-to' onClick={e => this.toParent(e, parent.id)}>
+                                Replying to @{parent.author}
+                            </button>
+                        )}
+                        <p>{text}</p>
+                    </div>
+                    <div className='tweet-icons'>
+                        <TiArrowBackOutline className='tweet-icon'/>
+                        <span>{replies !== 0 && replies}</span>
+                        <button className='heart-button' onClick={this.handleLike}>
+                            {hasLiked === true ?
+                            <TiHeartFullOutline color="#e0620a" className='tweet-icon'/>
+                            : <TiHeartOutline className='tweet-icon'/>}
+                        </button>
+                        <span>{likes !== 0 && likes}</span>
+                    </div>
+                </div>
             </div>
         )
     }
