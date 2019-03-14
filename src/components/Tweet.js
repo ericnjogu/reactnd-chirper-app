b6dd4eed1b9635dd102cd5fa8b6e547293d9f747
Eric Njogu <kunadawa@gmail.com>
2019-03-14 08:00:52 +0300

    feat: creating tweet component and using it in the dashboard component

---
+++ src/components/Tweet.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {formatTweet} from '../utils/helpers'
+
+class Tweet extends Component {
+    render() {
+        return (
+            <div className='tweet'>
+
+            </div>
+        )
+    }
+}
+
+function mapStateToProps({authedUser, users, tweets}, {id}) {
+    const tweet = tweets[id]
+    return {
+        authedUser,
+        tweet: formatTweet(tweet, users[tweet.author], authedUser, tweets[tweet.replyingTo])
+    }
+}
+
+export default connect(mapStateToProps)(Tweet)
