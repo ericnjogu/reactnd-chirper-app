1a92d82027940c505c14d87e599de5317e8d585e
Eric Njogu <kunadawa@gmail.com>
2019-03-26 11:13:23 +0300

    feat: navigation between tweet list and new tweet

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import {connect} from 'react-redux'
 import  {handleInitialData} from "../actions/shared";
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
 import NewTweet from "./NewTweet";
 import TweetPage from './TweetPage'
+import Nav from './Nav'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
   componentDidMount() {
@@ -13,13 +15,22 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-          <LoadingBar/>
-          {this.props.loading
-              ? null
-              : <TweetPage match={{params: {id: '6h5ims9iks66d4m7kqizmv'}}}/> //<NewTweet/> //<Dashboard/>
-          }
-      </div>
+        <Router>
+              <Fragment>
+                  <LoadingBar/>
+                  <div className='container'>
+                      <Nav/>
+                      {this.props.loading
+                          ? null
+                          : <div>
+                          <Route path='/' exact component={Dashboard}/>
+                          <Route path='/tweet/:id' component={TweetPage }/>
+                          <Route path='/new' component={NewTweet}/>
+                          </div>
+                      }
+                  </div>
+              </Fragment>
+      </Router>
     )
   }
 }
